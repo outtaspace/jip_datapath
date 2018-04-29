35dba3b3e8e122cfdee08b5211d2ebf9def1549a
outtaspace <flyweight@yandex.ru>
2018-04-29 10:48:50 +0300

    Improved documentation.

--- lib/JIP/DataPath.pm
+++ lib/JIP/DataPath.pm
@@ -120,3 +120,170 @@ sub _accessor {
 
 1;
 
+__END__
+
+=head1 NAME
+
+JIP::DataPath - provides a way to access data elements in a deep, complex and nested data structure.
+
+=head1 VERSION
+
+This document describes L<JIP::DataPath> version C<0.01>.
+
+=head1 SYNOPSIS
+
+    use JIP::DataPath qw(path);
+
+    path({foo => 42})->get(['foo']); # 42
+
+    path({foo => 42})->contains(['foo']); # True
+
+    if (path({foo => 42})->set(['foo'], 100500)) {
+        path({foo => 42})->get(['foo']); # 100500
+    }
+
+=head1 ATTRIBUTES
+
+L<JIP::DataPath> implements the following attributes.
+
+=head2 document
+
+    my $document = $pointer->document;
+
+Data structure to be processed.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 new
+
+    my $data_path = JIP::DataPath->new(document => {foo => 'bar'});
+
+Build new L<JIP::DataPath> object.
+
+=head2 get
+
+    # undef
+    JIP::DataPath->new(document => undef)->get([]);
+
+    # 42
+    JIP::DataPath->new(document => 42)->get([]);
+
+    # {foo => 'bar'}
+    JIP::DataPath->new(document => {foo => 'bar'})->get([]);
+
+    # 'bar'
+    JIP::DataPath->new(document => {foo => 'bar'})->get(['foo']);
+
+    # 'bar'
+    JIP::DataPath->new(document => ['foo', 'bar'])->get([1]);
+
+Extract value from L</"document"> identified by the given path.
+
+=head2 set
+
+    # True
+    JIP::DataPath->new(document => undef)->set([], {foo => undef});
+    JIP::DataPath->new(document => {foo => undef})->set(['foo'], {bar => undef});
+    JIP::DataPath->new(document => {foo => {bar => undef}})->set(['foo', 'bar'], []);
+    JIP::DataPath->new(document => {foo => {bar => []}})->set(['foo', 'bar'], [undef]);
+    JIP::DataPath->new(document => {foo => {bar => [undef]}})->set(['foo', 'bar', 0], 42);
+
+Sets the value at the specified path.
+
+=head2 contains
+
+    # True
+    JIP::DataPath->new(document => undef)->contains([]);
+    JIP::DataPath->new(document => 42)->contains([]);
+    JIP::DataPath->new(document => {foo => 'bar'})->contains([]);
+    JIP::DataPath->new(document => {foo => 'bar'})->contains(['foo']);
+    JIP::DataPath->new(document => ['foo', 'bar'])->contains([1]);
+
+    # False
+    JIP::DataPath->new(document => {foo => 'bar'})->contains(['wtf']);
+    JIP::DataPath->new(document => ['foo', 'bar'])->contains([42]);
+
+Check if L</"document"> contains a value that can be identified with the given path.
+
+=head1 EXPORTABLE FUNCTIONS
+
+These functions are exported only by request.
+
+=head2 path
+
+    use JIP::DataPath;
+
+    JIP::DataPath::path({foo => 42})->get(['foo']);
+    JIP::DataPath::path({foo => 42})->set(['foo'], 100500);
+    JIP::DataPath::path({foo => 42})->contains(['foo']);
+
+or exported on demand via
+
+    use JIP::DataPath qw(path);
+
+    path({foo => 42})->get(['foo']);
+    path({foo => 42})->set(['foo'], 100500);
+    path({foo => 42})->contains(['foo']);
+
+Alias of C<< JIP::DataPath->new(document => {}) >>. It creates a L<JIP::DataPath> object.
+
+=head1 DIAGNOSTICS
+
+None.
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+L<JIP::DataPath> requires no configuration files or environment variables.
+
+=head1 SEE ALSO
+
+L<Data::Focus>, L<Data::PathSimple>, L<Data::SimplePath>
+
+=head1 AUTHOR
+
+Vladimir Zhavoronkov, C<< <flyweight at yandex.ru> >>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2018 Vladimir Zhavoronkov.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the the Artistic License (2.0). You may obtain a
+copy of the full license at:
+
+L<http://www.perlfoundation.org/artistic_license_2_0>
+
+Any use, modification, and distribution of the Standard or Modified
+Versions is governed by this Artistic License. By using, modifying or
+distributing the Package, you accept this license. Do not use, modify,
+or distribute the Package, if you do not accept this license.
+
+If your Modified Version has been derived from a Modified Version made
+by someone other than you, you are nevertheless required to ensure that
+your Modified Version complies with the requirements of this license.
+
+This license does not grant you the right to use any trademark, service
+mark, tradename, or logo of the Copyright Holder.
+
+This license includes the non-exclusive, worldwide, free-of-charge
+patent license to make, have made, use, offer to sell, sell, import and
+otherwise transfer the Package with respect to any patent claims
+licensable by the Copyright Holder that are necessarily infringed by the
+Package. If you institute patent litigation (including a cross-claim or
+counterclaim) against any party alleging that the Package constitutes
+direct or contributory patent infringement, then this Artistic License
+to you shall terminate on the date that such litigation is filed.
+
+Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
+AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
+THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
+YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
+CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
+CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+=cut
+
+
